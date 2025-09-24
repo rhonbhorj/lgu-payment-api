1c2f3925a3c95dba0b2cd449eb06eb76fb9c1961
Paul Cullen <paulcullen.developer@gmail.com>
2025-09-24 13:55:14 +0800

    Update Transaction.php

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -18,31 +18,42 @@ class Transaction extends CI_Controller
 
     private function generateReference($length = 16)
     {
-    return strtoupper(substr(str_shuffle(str_repeat(
-        '0123456789ABCDEFGHIJKLMNOPQRSTUVWXFYZabcdefghijklmnopqrstuvwxyz',
-        $length
-    )), 0, $length));
+        return strtoupper(substr(str_shuffle(str_repeat(
+            '0123456789ABCDEFGHIJKLMNOPQRSTUVWXFYZabcdefghijklmnopqrstuvwxyz',
+            $length
+        )), 0, $length));
     }
 
 
     public function dotransac()
     {
 
+        //data
         $reference = $this->generateReference(16);
+        $name          = trim($this->input->post('name', TRUE));
+        $amount        = trim($this->input->post('amount', TRUE));
+        $mobile_number = trim($this->input->post('mobile_number', TRUE));
+        $email         = trim($this->input->post('email', TRUE));
+
+        //for postback
+        $return_url         = base_url('/success');
+        $callback_url       = base_url('/success/postback');
+
+
         $postdata = [
             "endpoint" => "p2m-generateQR",
             "reference_number"   => $reference,
-            "return_url" => "https://example.com/success",
-            "callback_url"      => "https://example.com/success/postback",
+            "return_url" => $return_url,
+            "callback_url"      => $callback_url,
             "merchant_details" => [
-                "txn_amount" => "100",
+                "txn_amount" =>  $amount,
                 "method" => "dynamic",
                 "txn_type" => "1",
-                "name" => "john doe",
-                "mobile_number" => "09123456782"
+                "name" => $name,
+                "mobile_number" => $mobile_number
             ],
             "email_confirmation" => [
-                "email" => "your@gmail.com",
+                "email" => $email,
                 "auto" => "off"
             ]
         ];
