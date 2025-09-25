eef7689fdcc63ccfb2c66997ba19d6046583a501
Paul Cullen <paulcullen.developer@gmail.com>
2025-09-25 15:08:31 +0800

    update conncecion in api

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -33,13 +33,29 @@ class Transaction extends CI_Controller
     public function dotransac()
     {
         // Collect form inputs
+
+        $mobile = trim($this->input->post('mobile_number', TRUE));
+        if (!empty($mobile)) {
+            // Convert 09xxxxxxxxx → 639xxxxxxxxx
+            if (preg_match('/^09\d{9}$/', $mobile)) {
+                $mobile = '63' . substr($mobile, 1);
+            }
+        } else {
+            $mobile = "639000000000"; // fallback
+        }
+
+        $email = trim($this->input->post('email', TRUE));
+        if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = "dev@netglobalsolutions.net"; // fallback default
+        }
+
         $data = [
             "reference"      => $this->generateReference(16),
             "refid" => "REF-" . date('YmdHis'),
             "name"           => trim($this->input->post('name', TRUE)),
             "amount"         => trim($this->input->post('amount', TRUE)),
-            "mobile_number"  => trim($this->input->post('mobile_number', TRUE)),
-            "email"          => trim($this->input->post('email', TRUE)),
+            "mobile_number"  => $mobile,
+            "email"          => $email,
             "convience_fee"  => trim($this->input->post('convience_fee', TRUE)),
             "company"  => trim($this->input->post('company', TRUE)),
             "return_url"     => base_url('/success'),
@@ -56,18 +72,18 @@ class Transaction extends CI_Controller
 
         // $data['reference']
         $transaction = [
-            'trans_no'        => $apiData['reference_number'] ?? $data['reference'] ,
+            'trans_no'        => $apiData['reference_number'] ?? $data['reference'],
             'trans_payor'     => $data['name'],
-            'trans_mobile'    => $data['mobile_number'],
-            'trans_email'     => $data['email'],
+            'trans_mobile'    => '09771741876',
+            'trans_email'     => 'devs@netglobalsolutions.net',
             'trans_company'     => $data['company'],
             'trans_sub_total' => $data['amount'],
             'trans_conv_fee'  => $data['convience_fee'],
             'trans_refid'     => $data['refid'] ?? '',
             'trans_txid'      => $apiData['txn_ref'] ?? '',
-            'trans_ref'      =>$data['reference'] ?? '',
+            'trans_ref'      => $data['reference'] ?? '',
             'trans_raw_string' => $apiData['raw_string'] ?? '',   // ✅ store raw_string from API response
-            'trans_status'    =>'CREATED'
+            'trans_status'    => 'CREATED'
         ];
 
         // Save transaction
